f9334536d5cb8971aa513faa954a59ef3d13fb11
kkmintaddict <146964446+kkmintaddict@users.noreply.github.com>
2023-11-23 00:55:15 +0800

    test

--- js/all.js
+++ js/all.js
@@ -1,56 +1,53 @@
 $(document).ready(function() {
 
-    $(".img-change").hover(
-        function() {  
+    $(".img-change").hover(function() {  
             $(".drawMe").toggleClass('show');
         });
 
-    $(".designChange01").hover(
-        function() {  
+    $(".designChange01").hover(function() {  
             $("#chang01").toggleClass('hide');
         });
-    $(".designChange02").hover(
-        function() {  
+    $(".designChange02").hover(function() {  
             $("#chang02").toggleClass('hide');
         });
-    $(".designChange03").hover(
-        function() {  
+    $(".designChange03").hover(function() {  
             $("#chang03").toggleClass('hide');
+        });
+
+
+    //
+
+    $(".adoptChange01").hover(function() {  
+            $("#puppy01").toggleClass('hide');
+        });
+    
+    $(".adoptChange02").hover(function() {  
+            $("#puppy02").toggleClass('hide');
+        });
+
+    $(".adoptChange03").hover(function() {  
+            $("#puppy03").toggleClass('hide');
+        });
+        
+    $(".adoptChange04").hover(function() {  
+            $("#puppy04").toggleClass('hide');
+        });
+
+    $(".adoptChange05").hover(function() {  
+            $("#puppy05").toggleClass('hide');
+        });
+
+    $(".adoptChange06").hover(function() {  
+            $("#puppy06").toggleClass('hide');
         });
 
 
 
     //
+    $(".designer").hover(function() {  
+        $(this).addClass('active').siblings().removeClass('active');
+    });
 
-    $(".adoptChange01").hover(
-        function() {  
-            $("#puppy01").toggleClass('hide');
-        });
-    
-    $(".adoptChange02").hover(
-        function() {  
-            $("#puppy02").toggleClass('hide');
-        });
-
-    $(".adoptChange03").hover(
-        function() {  
-            $("#puppy03").toggleClass('hide');
-        });
-        
-    $(".adoptChange04").hover(
-        function() {  
-            $("#puppy04").toggleClass('hide');
-        });
-
-    $(".adoptChange05").hover(
-        function() {  
-            $("#puppy05").toggleClass('hide');
-        });
-
-    $(".adoptChange06").hover(
-        function() {  
-            $("#puppy06").toggleClass('hide');
-        });
     
     
 
